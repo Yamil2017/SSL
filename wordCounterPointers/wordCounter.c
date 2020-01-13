09f2fafe0121be442ae100a8e5e75961dd739e0c
Yamil Lopez <pechyar@hotmail.com>
2020-01-13 18:06:47 -0300

    wordCounter using self referenced structures

--- wordCounterPointers/wordCounter.c
+++ wordCounterPointers/wordCounter.c
@@ -12,6 +12,14 @@ NodeTree* root;
 root=NULL;
 char word[MAXWORD];
 
+/* Defino primer nodo del arbol, incializacion del primer nodo con addtree() no funciona 
+char* palabra="hola";
+root=talloc();
+root->count=1;
+root->word=ystrdup(palabra);
+root->leftNode=root->rightNode=NULL;
+*/
+
 while(getword(word,MAXWORD)!=EOF)
     if(isalpha(word[0]))
         addtree(root,word);
